085ef5df008d05563c946024527b08a78fc6f09a
yuyudifiesh <yizhixianyuuuu@gmail.com>
2026-03-16 19:23:40 +0800

    add

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <p class="read-the-docs">Copyright © 2026 Vilinko Studio</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
