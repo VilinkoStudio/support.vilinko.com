340a4cd37e0e563c1cea6ae5ba9d53ba17259255
yuyudifiesh <yizhixianyuuuu@gmail.com>
2026-03-17 23:29:44 +0800

    add

---
+++ .vitepress/config.mjs
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  base: "",
+  title: "Vilinko Support",
+  description: "Vilinko Studio 用户支持中心，提供多种渠道的用户支持方式，帮助解决问题",
+  keywords: 'Vilinko Studio,支持,产品,服务,用户,Vilinko,vertillusion,lightframe,lfs,vinaui,vui,support',
+  locales: {
+    root: {
+      label: '简体中文',
+      lang: 'zh-CN',
+      themeConfig: {
+        nav: [
+          { text: '首页', link: '/' },
+        ],
+        footer: {
+          copyright: 'Copyright © 2019 - 2026 Vilinko Studio. All Rights Reserved.',
+          message: '<a href="https://beian.miit.gov.cn/" target="_blank" style="text-decoration: none;">粤ICP备2025454076号</a>'
+        }
+      }
+    },
+    en: {
+      label: 'English',
+      lang: 'en-US',
+      link: '/en/',
+      themeConfig: {
+        nav: [
+          { text: 'Home', link: '/en/' },
+        ],
+        footer: {
+          copyright: 'Copyright © 2019 - 2026 Vilinko Studio. All Rights Reserved.',
+          message: '<a href="https://beian.miit.gov.cn/" target="_blank" style="text-decoration: none;">粤ICP备2025454076号</a>'
+        }
+      }
+    }
+  },
+
+  themeConfig: {
+    sidebar: false,
+    aside: "left",
+  }
+})
---
+++ en/index.md
@@ -0,0 +1,21 @@
+---
+layout: home
+
+hero:
+  text: "SUPPORT"
+  actions:
+    - theme: brand
+      text: Markdown Examples
+      link: /markdown-examples
+    - theme: alt
+      text: API Examples
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
