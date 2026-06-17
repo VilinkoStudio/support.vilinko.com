b4c0ec2d9dd603a00b967844fedc0d3652768ce2
yuyudifiesh <yizhixianyuuuu@gmail.com>
2026-06-18 00:37:38 +0800

    add

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -66,5 +66,23 @@ export default defineConfig({
 
   themeConfig: {
     sidebar: false,
+
+    socialLinks: [
+      { icon: 'xiaohongshu', link: 'https://www.xiaohongshu.com/user/profile/68fa354900000000320280e5' },
+      { icon: 'qq', link: 'https://pd.qq.com/s/fnd9tarkb' },
+      { icon: 'wechat', link: 'https://support.vilinko.com/wechat.htm' },
+      {
+        icon: {
+          svg: '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Google Docs</title><path d="M14.727 6.727H14V0H4.91c-.905 0-1.637.732-1.637 1.636v20.728c0 .904.732 1.636 1.636 1.636h14.182c.904 0 1.636-.732 1.636-1.636V6.727h-6zm-.545 10.455H7.09v-1.364h7.09v1.364zm2.727-3.273H7.091v-1.364h9.818v1.364zm0-3.273H7.091V9.273h9.818v1.363zM14.727 6h6l-6-6v6z"/></svg>'
+        },
+        link: 'https://docs.vilinko.com'
+      },
+      {
+        icon: {
+          svg: '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Gmail</title><path d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z"/></svg>'
+        },
+        link: 'https://support.vilinko.com/email.htm'
+      }
+    ],
   },
 })
